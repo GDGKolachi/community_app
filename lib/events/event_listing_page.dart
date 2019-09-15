cece7b2b4c8096effa448020af07987fdad63118
aubykhan <aubykhan@gmail.com>
2019-09-15 16:12:45 +0500

    Move move event drawer stuff to new file

--- lib/events/event_listing_page.dart
+++ lib/events/event_listing_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_pk/events/event_detail_container.dart';
 import 'package:flutter_pk/events/model.dart';
 import 'package:flutter_pk/global.dart';
 import 'package:flutter_pk/helpers/formatters.dart';
-import 'package:flutter_pk/profile/profile_dialog.dart';
+import 'package:flutter_pk/widgets/navigation_drawer.dart';
 
 class EventListingPage extends StatelessWidget {
   final EventApi api = new EventApi();
@@ -106,72 +106,6 @@ class EventListItem extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class NavigationDrawerItems extends StatelessWidget {
-  const NavigationDrawerItems({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        DrawerHeader(
-          child: Image.asset('assets/gdg_kolachi.png'),
-        ),
-        _buildNavItem(
-          context,
-          Icons.home,
-          'Home',
-        ),
-        _buildNavItem(
-          context,
-          Icons.event,
-          'Events',
-        ),
-        _buildNavItem(
-          context,
-          Icons.info,
-          'About',
-        ),
-        _buildNavItem(
-          context,
-          Icons.notifications,
-          'Notifications',
-        ),
-        _buildNavItem(
-          context,
-          Icons.person,
-          'My profile',
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => FullScreenProfileDialog(),
-              fullscreenDialog: true,
-            ),
-          ),
-        ),
-        _buildNavItem(
-          context,
-          Icons.exit_to_app,
-          'Log out',
-        ),
-      ],
-    );
-  }
-
-  ListTile _buildNavItem(BuildContext context, IconData icon, String title,
-      {VoidCallback onTap}) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(title),
-      onTap: () {
-        Navigator.pop(context);
-        if (onTap != null) onTap();
-      },
     );
   }
 }
---
+++ lib/widgets/navigation_drawer.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pk/profile/profile_dialog.dart';
+
+class NavigationDrawerItems extends StatelessWidget {
+  const NavigationDrawerItems({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        DrawerHeader(
+          child: Image.asset('assets/gdg_kolachi.png'),
+        ),
+        _buildNavItem(
+          context,
+          Icons.home,
+          'Home',
+        ),
+        _buildNavItem(
+          context,
+          Icons.event,
+          'Events',
+        ),
+        _buildNavItem(
+          context,
+          Icons.info,
+          'About',
+        ),
+        _buildNavItem(
+          context,
+          Icons.notifications,
+          'Notifications',
+        ),
+        _buildNavItem(
+          context,
+          Icons.person,
+          'My profile',
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FullScreenProfileDialog(),
+              fullscreenDialog: true,
+            ),
+          ),
+        ),
+        _buildNavItem(
+          context,
+          Icons.exit_to_app,
+          'Log out',
+        ),
+      ],
+    );
+  }
+
+  ListTile _buildNavItem(BuildContext context, IconData icon, String title,
+      {VoidCallback onTap}) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        Navigator.pop(context);
+        if (onTap != null) onTap();
+      },
+    );
+  }
+}
