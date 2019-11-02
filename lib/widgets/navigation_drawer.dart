16e3d025058de565ea902bd17485acfe91c81f0a
aubykhan <aubykhan@gmail.com>
2019-11-02 17:33:36 +0500

    Add about dialog

--- lib/widgets/navigation_drawer.dart
+++ lib/widgets/navigation_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pk/profile/profile_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NavigationDrawerItems extends StatelessWidget {
   const NavigationDrawerItems({
@@ -18,11 +19,7 @@ class NavigationDrawerItems extends StatelessWidget {
           Icons.home,
           'Home',
         ),
-        _buildNavItem(
-          context,
-          Icons.info,
-          'About',
-        ),
+        _buildAboutItem(),
         _buildNavItem(
           context,
           Icons.person,
@@ -37,6 +34,39 @@ class NavigationDrawerItems extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  AboutListTile _buildAboutItem() {
+    return AboutListTile(
+        applicationIcon: Image.asset(
+          'assets/ic_gdg_app.png',
+          height: 40,
+        ),
+        applicationName: 'GDG Kolachi',
+        icon: Icon(Icons.info),
+        aboutBoxChildren: <Widget>[
+          Text('Google Developers Group Kolachi for Karachi'),
+          SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Image.asset('assets/facebook.png'),
+                onPressed: () => launch('https://www.facebook.com/GDGKolachi'),
+              ),
+              IconButton(
+                icon: Image.asset('assets/twitter.png'),
+                onPressed: () => launch('https://www.twitter.com/GDGKolachi'),
+              ),
+              IconButton(
+                icon: Image.asset('assets/instagram.png'),
+                onPressed: () => launch('https://www.instagram.com/GDGKolachi'),
+              ),
+            ],
+          ),
+        ],
+        applicationVersion: '1.0',
+      );
   }
 
   ListTile _buildNavItem(BuildContext context, IconData icon, String title,
